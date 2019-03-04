192587093b87394e5a243566d002cdbe2636047e
Schmitda <daniel.schmitz@schmitz-dynamics.ch>
2019-03-04 19:30:33 +0100

    finished version

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename(req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+const gm = require("gm");
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/api/files', upload.single('file'), (req, res) => {
+    gm(req.file.path)
+        .resize(720)
+        .write('uploads/small_' + req.file.filename, () => {
+    });
+    gm(req.file.path)
+        .resize(1280)
+        .write('uploads/medium_' + req.file.filename, () => {
+    });
+    gm(req.file.path)
+        .resize(2044)
+        .write('uploads/big_' + req.file.filename, () => {
+    });
+    res.status(200).json({ success: true });
+});
+app.use('/static', express.static('uploads'));
 app.listen(process.env.PORT || 4000, function () {
     console.log('Your node js server is running');
 });

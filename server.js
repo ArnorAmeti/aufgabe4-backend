a94703162e367b1cbf5470c67bf4d2b992530152
Schmitda <daniel.schmitz@schmitz-dynamics.ch>
2019-03-04 19:09:56 +0100

    Heroku env port

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Your node js server is running');
+});
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
